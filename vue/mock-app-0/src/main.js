62a64e9e8a0c97e6a390daac7bc770571e7e96e8
30677472@qq.com <30677472@qq.com>
2020-03-11 19:51:50 +0800

    test

--- vue/mock-app-0/src/main.js
+++ vue/mock-app-0/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './plugins/element.js'
-import './mock/'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import '@/plugins/element.js'
+import '@/mock'
 
 Vue.config.productionTip = false
 
