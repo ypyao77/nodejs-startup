51c800fb6f54f9f3377c4a1f704faee71cb89f6c
ypyao77 <30677472@qq.com>
2020-03-14 08:55:18 +0800

    test

--- vue/mix-app-0/babel.config.js
+++ vue/mix-app-0/babel.config.js
@@ -1,4 +1,12 @@
+let transformRemoveConsolePlugin = []
+if (process.env.NODE_ENV === 'production') {
+  transformRemoveConsolePlugin = "transform-remove-console"
+}
+
 module.exports = {
+  plugins: [
+    transformRemoveConsolePlugin
+  ],
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
