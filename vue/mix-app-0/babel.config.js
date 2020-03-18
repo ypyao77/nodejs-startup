31eb62db40f40d26a1681bddf7f918e931abca52
30677472@qq.com <30677472@qq.com>
2020-03-18 15:24:23 +0800

    charts

--- vue/mix-app-0/babel.config.js
+++ vue/mix-app-0/babel.config.js
@@ -1,11 +1,4 @@
-let transformRemoveConsolePlugin = []
-if (process.env.NODE_ENV === 'production') {
-  transformRemoveConsolePlugin = ["transform-remove-console"]
-}
-
 module.exports = {
-  plugins: ["transform-remove-console"],
-
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
--- vue/mix-app-0/src/App.vue
+++ vue/mix-app-0/src/App.vue
@@ -2,9 +2,6 @@
   <div id="app">
     <img src="./assets/logo.png">
     <el-row>
-      <p> process.env.BASE_URL: {{ burl }} process.env.VUE_APP_BASE_URL: {{ vburl }} </p>
-    </el-row>
-    <el-row>
       <router-link :to="{ path: '/views/env' }">表格</router-link> ||
       <router-link :to="{ path: '/views/button' }">按钮</router-link> ||
       <router-link :to="{ path: '/views/badge' }">标记</router-link> ||
@@ -15,9 +12,9 @@
       <router-link :to="{ path: '/views/scroll' }">滚动显示</router-link> ||
       <router-link :to="{ path: '/views/dialog' }">对话框</router-link> ||
       <router-link :to="{ path: '/views/layout' }">layout布局</router-link> ||
+      <router-link :to="{ path: '/views/form' }">表单</router-link> ||
       <router-link :to="{ path: '/views/vcharts' }">V图表</router-link> ||
       <router-link :to="{ path: '/views/echarts' }">E图表</router-link> ||
-      <router-link :to="{ path: '/views/form' }">表单</router-link> ||
     </el-row>
 
     <el-divider></el-divider>
@@ -45,7 +42,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
